ea138d861b331b2454198dfe5332bcbb0ffad757
Transformer075 <haseebiftikhar452@gmail.com>
2021-11-07 19:21:25 +0500

    Practice Intent

--- app/src/main/java/com/example/practicequizapp/FirstActivity.java
+++ app/src/main/java/com/example/practicequizapp/FirstActivity.java
@@ -2,16 +2,40 @@ package com.example.practicequizapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class FirstActivity extends AppCompatActivity {
+
+    Button btn_Practice;
+    Button btn_Quiz;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
 
+        btn_Practice = findViewById(R.id.btn_secondActivity);
+        btn_Quiz = findViewById(R.id.btn_thirdActivity);
+
+        Intent intent;
+        btn_Practice.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(FirstActivity.this, activity_second.class);
+                startActivity(intent);
+            }
+        });
+        btn_Quiz.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
 
 
     }
