98208a168f3ea7b1d2f2ac849bdaeecb0d1e8ed4
Transformer075 <haseebiftikhar452@gmail.com>
2021-12-06 18:50:04 +0500

    Menu Items Action added

--- app/src/main/java/com/example/practicequizapp/FirstActivity.java
+++ app/src/main/java/com/example/practicequizapp/FirstActivity.java
@@ -8,6 +8,7 @@ import android.media.Image;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -57,5 +58,26 @@ public class FirstActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu){
         getMenuInflater().inflate(R.menu.menu, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case R.id.homeButton:
+                Intent intent1 = new Intent(this, FirstActivity.class);
+                startActivity(intent1);
+                break;
+            case R.id.practiceButton:
+                Intent intent2 = new Intent(this, activity_second.class);
+                startActivity(intent2);
+                break;
+            case R.id.quizButton:
+                Intent intent3 = new Intent(this, activity_three.class);
+                startActivity(intent3);
+                break;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+        return false;
     }
 }
